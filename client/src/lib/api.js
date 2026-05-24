db4d82c2d7e86797dff6d50745a795c7f0deeadc
vishnusharma17 <vishnusharma983j@gmail.com>
2026-05-24 23:11:37 +0530

    ye lo commit

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL || "https://b2b-narayanam.onrender.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5004/api",
 });
 
 export default API;
