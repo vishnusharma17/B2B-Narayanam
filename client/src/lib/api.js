eb3d01806890dbcbf0dd160899bea1bf9e88527d
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-07 14:00:02 +0530

    login

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "${BASE_URL}/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("adminToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default API;
