28d6e9f59bb25e12ae6fb86ecb21911d7c931ac2
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-07 14:10:07 +0530

    new pages

---
+++ client/src/app/shipping-policy/page.jsx
@@ -0,0 +1,13 @@
+export default function ShippingPolicy() {
+  return (
+    <div className="max-w-5xl mx-auto py-20 px-6">
+      <h1 className="text-4xl font-bold mb-6">
+        Shipping Policy
+      </h1>
+
+      <p>
+        Shipping policy content here...
+      </p>
+    </div>
+  );
+}
---
+++ client/src/app/terms/page.jsx
@@ -0,0 +1,13 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="max-w-5xl mx-auto py-20 px-6">
+      <h1 className="text-4xl font-bold mb-6">
+        Privacy Policy
+      </h1>
+
+      <p>
+        Your privacy policy content here...
+      </p>
+    </div>
+  );
+}
