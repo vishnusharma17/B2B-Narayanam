e6886cdb311b330043da30b62ea8d3d4fa659b95
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-09 23:21:16 +0530

    page optemizes done

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,17 +2,19 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, text }) => {
   try {
-    console.log("EMAIL USER:", process.env.EMAIL_USER);
-
-    console.log("EMAIL PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
-
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
+
+    // 👇 Yaha add kar
+    await transporter.verify();
+    console.log("SMTP Connected");
 
     console.log("Sending email to:", to);
 
