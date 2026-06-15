d1822a4efc8bed1428484805150016468e4e7ac1
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-15 11:30:10 +0530

    email fixing

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -12,9 +12,6 @@ export const sendEmail = async ({ to, subject, text }) => {
       },
     });
 
-    await transporter.verify();
-    console.log("SMTP Connected");
-
     // 👇 Yaha add kar
     await transporter.verify();
     console.log("SMTP Connected");
