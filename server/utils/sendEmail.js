1d5a8a6435d779d3996909b1e4ceef40a6b92ece
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-15 12:36:40 +0530

    final fixess

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,17 +9,11 @@ export const sendEmail = async ({ to, subject, text }) => {
     console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4, // force IPv4
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     });
 
     const info = await transporter.sendMail({
