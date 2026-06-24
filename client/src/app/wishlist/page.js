27decdd5ce259fcde9c5250ba5dc7a4892848181
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-24 22:15:32 +0530

    wishlist fixes

--- client/src/app/wishlist/page.js
+++ client/src/app/wishlist/page.js
@@ -333,7 +333,7 @@ export default function WishlistPage() {
     transition-all
   "
                     >
-                      Remove
+                      X
                     </button>
                   </div>
                 ))}
