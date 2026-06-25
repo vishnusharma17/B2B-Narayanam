c1a5ec44ff4dd4053f9fd4a7ea67bb0a8beb87de
vishnusharma17 <vishnusharma983j@gmail.com>
2026-06-25 15:34:46 +0530

    password problem fix

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,7 +9,9 @@ export const sendEmail = async ({ to, subject, text }) => {
     console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
